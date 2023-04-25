99f26060c4d5c6dfbc6e3332d8bf5c9d88e58b15
S559228-Mounica <seelamgususspring2023@gmail.com>
2023-04-25 17:30:14 -0500

    ss5

--- routes/shirt.js
+++ routes/shirt.js
@@ -10,9 +10,15 @@ router.get('/detail', shirt_controlers.shirt_view_one_Page);
 
 /* GET create shirt page */
 router.get('/create', shirt_controlers.shirt_create_Page);
-
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}   
 /* GET create update page */
-router.get('/update', shirt_controlers.shirt_update_Page);
+router.get('/update',secured, shirt_controlers.shirt_update_Page);
 
 /* GET delete shirt page */
 router.get('/delete', shirt_controlers.shirt_delete_Page);
